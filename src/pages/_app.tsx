48bf2521d62dd115fb5a443db35a357668169fd3
leovenom <leonardt@outlook.com>
2021-06-20 18:03:08 +0200

    done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,40 +2,20 @@ import '../styles/global.scss';
 import styles from '../styles/app.module.scss';
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
-import { PlayerContext } from '../contexts/PlayerContext';
-import { useState } from 'react';
+import { PlayerContextProvider } from '../contexts/PlayerContext'
 
 function MyApp({ Component, pageProps }) {
-    const [episodeList, setEpisodeList] = useState([]);
-    const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-    const [isPlaying, setIsPlaying] = useState(false);
-
-    function play(episode) {
-        setEpisodeList([episode]);
-        setCurrentEpisodeIndex(0);
-        setIsPlaying(true);
-    };
-    // togglePlay - se ele tiver tocando eu pauso se ele tiver pausado eu toco.
-    function togglePlay() {
-                    // trocar o valor para o contrario dela !(negação)
-        setIsPlaying(!isPlaying);
-    };
-
-    function setPlayingState(state:boolean) {
-        setIsPlaying(state)
-    }
     return (
-        <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
+        <PlayerContextProvider>
             <div className={ styles.wrapper}>
                 <main>
                     <Header />
                     <Component {...pageProps} />
                 </main>
-                    <Player/>
+                <Player/>
             </div>
-        </PlayerContext.Provider>
-    )
-    
+        </PlayerContextProvider>
+    ) 
     
 }
 
