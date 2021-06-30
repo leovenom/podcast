9196ee8a253566ec24a3e40f2287974b76b84c76
leovenom <leonardt@outlook.com>
2021-06-30 16:57:34 +0200

    ok

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,5 +3,5 @@ import axios from 'axios';
 
 
 export const api = axios.create({
-    baseURL: process.env.API_URL || 'https://github.com/leovenom/podcast/blob/main/db.json'
+    baseURL: process.env.API_URL || 'http://localhost:3333'
   })
