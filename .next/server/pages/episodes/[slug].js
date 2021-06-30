80d64383cd4225482214f827b623674ac59ca6fd
leovenom <leonardt@outlook.com>
2021-06-30 16:03:50 +0200

    json-server

---
+++ .next/server/pages/episodes/[slug].js
@@ -0,0 +1,272 @@
+(function() {
+var exports = {};
+exports.id = 342;
+exports.ids = [342];
+exports.modules = {
+
+/***/ 1083:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ Episode; },
+/* harmony export */   "getStaticPaths": function() { return /* binding */ getStaticPaths; },
+/* harmony export */   "getStaticProps": function() { return /* binding */ getStaticProps; }
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5282);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var date_fns__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3879);
+/* harmony import */ var date_fns__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(date_fns__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var date_fns_locale_pt_BR__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(8994);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5675);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(701);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(1664);
+/* harmony import */ var _contexts_PlayerContext__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(1983);
+/* harmony import */ var _services_api__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(1224);
+/* harmony import */ var _utils_convertDurationToTimeString__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(4005);
+/* harmony import */ var _episode_module_scss__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(8904);
+/* harmony import */ var _episode_module_scss__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(_episode_module_scss__WEBPACK_IMPORTED_MODULE_7__);
+
+
+
+
+
+
+
+
+
+
+
+function Episode({
+  episode
+}) {
+  const {
+    play
+  } = (0,_contexts_PlayerContext__WEBPACK_IMPORTED_MODULE_5__/* .usePlayer */ .nn)();
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+    className: (_episode_module_scss__WEBPACK_IMPORTED_MODULE_7___default().episode),
+    children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_head__WEBPACK_IMPORTED_MODULE_3___default()), {
+      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("title", {
+        children: [episode.title, "| Podcast"]
+      })
+    }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+      className: (_episode_module_scss__WEBPACK_IMPORTED_MODULE_7___default().thumbnailContainer),
+      children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_link__WEBPACK_IMPORTED_MODULE_4__.default, {
+        href: "/",
+        children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("button", {
+          type: "button",
+          children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+            src: "/arrow-left.svg",
+            alt: "Voltar"
+          })
+        })
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_image__WEBPACK_IMPORTED_MODULE_2__.default, {
+        width: 700,
+        height: 160,
+        src: episode.thumbnail,
+        objectFit: "cover"
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("button", {
+        type: "button",
+        onClick: () => play(episode),
+        children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+          src: "/play.svg",
+          alt: "Tocar epis\xF3dio"
+        })
+      })]
+    }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("header", {
+      children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h1", {
+        children: episode.title
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+        children: episode.members
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+        children: episode.publishedAt
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+        children: episode.durationAsString
+      })]
+    }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+      className: (_episode_module_scss__WEBPACK_IMPORTED_MODULE_7___default().description),
+      dangerouslySetInnerHTML: {
+        __html: episode.description
+      }
+    })]
+  });
+}
+const getStaticPaths = async () => {
+  const {
+    data
+  } = await _services_api__WEBPACK_IMPORTED_MODULE_6__/* .api.get */ .h.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    };
+  });
+  return {
+    paths,
+    fallback: 'blocking'
+  };
+};
+const getStaticProps = async ctx => {
+  const {
+    slug
+  } = ctx.params;
+  const {
+    data
+  } = await _services_api__WEBPACK_IMPORTED_MODULE_6__/* .api.get */ .h.get(`/episodes/${slug}`);
+  const episode = {
+    id: data.id,
+    title: data.title,
+    thumbnail: data.thumbnail,
+    members: data.members,
+    publishedAt: (0,date_fns__WEBPACK_IMPORTED_MODULE_1__.format)((0,date_fns__WEBPACK_IMPORTED_MODULE_1__.parseISO)(data.published_at), 'd MMM yy', {
+      locale: date_fns_locale_pt_BR__WEBPACK_IMPORTED_MODULE_8__/* .default */ .Z
+    }),
+    duration: Number(data.file.duration),
+    durationAsString: (0,_utils_convertDurationToTimeString__WEBPACK_IMPORTED_MODULE_9__/* .convertDurationToTimeString */ .D)(Number(data.file.duration)),
+    description: data.description,
+    url: data.file.url
+  };
+  return {
+    props: {
+      episode
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
+
+  };
+};
+
+/***/ }),
+
+/***/ 1224:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "h": function() { return /* binding */ api; }
+});
+
+;// CONCATENATED MODULE: external "axios"
+var external_axios_namespaceObject = require("axios");;
+var external_axios_default = /*#__PURE__*/__webpack_require__.n(external_axios_namespaceObject);
+;// CONCATENATED MODULE: ./src/services/api.ts
+
+const api = external_axios_default().create({
+  baseURL: process.env.API_URL || 'http://localhost:3333'
+});
+
+/***/ }),
+
+/***/ 8904:
+/***/ (function(module) {
+
+// Exports
+module.exports = {
+	"episode": "episode_episode__3xSMv",
+	"thumbnailContainer": "episode_thumbnailContainer__g4oD1",
+	"description": "episode_description__3hw48"
+};
+
+
+/***/ }),
+
+/***/ 3879:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("date-fns");;
+
+/***/ }),
+
+/***/ 5273:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/dist/next-server/lib/head.js");;
+
+/***/ }),
+
+/***/ 8417:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/dist/next-server/lib/router-context.js");;
+
+/***/ }),
+
+/***/ 2238:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/dist/next-server/lib/router/utils/get-asset-path-from-route.js");;
+
+/***/ }),
+
+/***/ 5519:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/dist/next-server/lib/to-base-64.js");;
+
+/***/ }),
+
+/***/ 444:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/dist/next-server/server/image-config.js");;
+
+/***/ }),
+
+/***/ 701:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/head");;
+
+/***/ }),
+
+/***/ 9297:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("react");;
+
+/***/ }),
+
+/***/ 5282:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("react/jsx-runtime");;
+
+/***/ }),
+
+/***/ 4453:
+/***/ (function() {
+
+/* (ignored) */
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+var __webpack_exports__ = __webpack_require__.X(0, [943,999,664,543], function() { return __webpack_exec__(1083); });
+module.exports = __webpack_exports__;
+
+})();
